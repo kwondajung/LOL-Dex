ae8ff6a19ab066159114383bed3bb7d052bfaba4
dajung <kwonkkan@naver.com>
2024-10-02 05:05:38 +0900

    ✨ Feat: 챔피언 name, title 가져오기

--- src/app/champions/page.tsx
+++ src/app/champions/page.tsx
@@ -2,13 +2,31 @@ import { getChampion } from '@/utils/serverApi';
 
 const page = async () => {
   const championData = await getChampion();
-  console.log(championData);
 
-  // TODO: 배열로 바꾸기 => map 활용
+  // 객체 데이터를 배열로 변환
+  const championArr = Object.entries(championData.data);
+
+  // TODO: champion unknown 뜨는 거 수정
+  const championList = championArr.map(([key, champion]) => {
+    return {
+      name: champion.name,
+      title: champion.title,
+    };
+  });
+
+  console.log(championList);
 
   return (
     <>
-      <div>챔피언 목록 페이지</div>
+      <div>챔피언 목록</div>
+      <div className="grid grid-cols-4 gap-4">
+        {championList.map((champion, index) => (
+          <div key={index}>
+            <p>{champion.name}</p>
+            <p>{champion.title}</p>
+          </div>
+        ))}
+      </div>
     </>
   );
 };
