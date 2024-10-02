ed05c9149942e8ae021bb76a29e064c75180ecee
dajung <kwonkkan@naver.com>
2024-10-03 03:18:39 +0900

    💄 Style: 챔피언 목록 이미지 변경

--- src/app/champions/page.tsx
+++ src/app/champions/page.tsx
@@ -1,5 +1,6 @@
 import { getChampion } from '@/utils/serverApi';
 import Image from 'next/image';
+import Link from 'next/link';
 
 const page = async () => {
   const championData = await getChampion();
@@ -17,26 +18,28 @@ const page = async () => {
     };
   });
 
-  console.log(championList);
+  // console.log(championList);
 
   return (
     <>
       <h1 className="text-2xl font-bold text-center mb-5">챔피언 목록</h1>
-      <div className="grid grid-cols-4 gap-4">
+      <div className="grid grid-cols-6 gap-4">
         {championList.map((champion, index) => (
-          <div
-            key={index}
-            className="grid place-items-center border text-center"
-          >
-            <Image
-              src={`https://ddragon.leagueoflegends.com/cdn/14.19.1/img/champion/${champion.id}.png`}
-              alt={champion.name}
-              width={100}
-              height={100}
-            />
-            <p>{champion.name}</p>
-            <p>{champion.title}</p>
-          </div>
+          <Link href={`/champions/${champion.id}`}>
+            <div
+              key={index}
+              className="grid place-items-center border text-center p-5"
+            >
+              <Image
+                src={`https://ddragon.leagueoflegends.com/cdn/img/champion/loading/${champion.id}_0.jpg`}
+                alt={champion.name}
+                width={200}
+                height={100}
+              />
+              <p>{champion.name}</p>
+              <p>{champion.title}</p>
+            </div>
+          </Link>
         ))}
       </div>
     </>
