a5d18e7d886825b1848001c4394b17ef54057a87
dajung <kwonkkan@naver.com>
2024-10-02 15:40:18 +0900

    :sparkles: Feat: 챔피언 이미지 불러오기

--- src/app/champions/page.tsx
+++ src/app/champions/page.tsx
@@ -1,4 +1,5 @@
 import { getChampion } from '@/utils/serverApi';
+import Image from 'next/image';
 
 const page = async () => {
   const championData = await getChampion();
@@ -9,8 +10,10 @@ const page = async () => {
   // TODO: champion unknown 뜨는 거 수정
   const championList = championArr.map(([key, champion]) => {
     return {
+      id: champion.id,
       name: champion.name,
       title: champion.title,
+      image: champion.image.full,
     };
   });
 
@@ -18,10 +21,19 @@ const page = async () => {
 
   return (
     <>
-      <div>챔피언 목록</div>
+      <h1 className="text-2xl font-bold text-center mb-5">챔피언 목록</h1>
       <div className="grid grid-cols-4 gap-4">
         {championList.map((champion, index) => (
-          <div key={index}>
+          <div
+            key={index}
+            className="grid place-items-center border text-center"
+          >
+            <Image
+              src={`https://ddragon.leagueoflegends.com/cdn/14.19.1/img/champion/${champion.id}.png`}
+              alt={champion.name}
+              width={100}
+              height={100}
+            />
             <p>{champion.name}</p>
             <p>{champion.title}</p>
           </div>
