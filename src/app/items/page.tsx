8f39cf0b920775c3a224ab0b340d8566280a247c
dajung <kwonkkan@naver.com>
2024-10-04 03:40:13 +0900

    ✨ Feat: 아이템 목록 불러오기

--- src/app/items/page.tsx
+++ src/app/items/page.tsx
@@ -1,5 +1,41 @@
-const page = () => {
-  return <div>아이템 목록 페이지</div>;
+import { getItem } from '@/utils/serverApi';
+import Image from 'next/image';
+
+const page = async () => {
+  const ItemData = await getItem();
+
+  const itemArr = Object.entries(ItemData.data);
+
+  const ItemList = itemArr.map(([key, item]) => {
+    return {
+      id: key,
+      name: item.name,
+      plaintext: item.plaintext,
+    };
+  });
+
+  return (
+    <>
+      <h1 className="text-2xl font-bold text-center mb-5">아이템 목록</h1>
+      <div className="grid grid-cols-6 gap-4">
+        {ItemList.map((item, index) => (
+          <div
+            key={index}
+            className='grid place-items-center border text-center p-5"'
+          >
+            <Image
+              src={`https://ddragon.leagueoflegends.com/cdn/14.19.1/img/item/${item.id}.png`}
+              alt={item.name}
+              width={100}
+              height={100}
+            />
+            <p>{item.name}</p>
+            <p>{item.plaintext}</p>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default page;
--- src/utils/serverApi.ts
+++ src/utils/serverApi.ts
@@ -1,4 +1,4 @@
-'use server';
+// 'use server';
 
 // TODO: return 값에 대한 타입 지정
 // 최신 버전 가져오기
@@ -29,7 +29,7 @@ export const getChampion = async () => {
 
     return data;
   } catch (error) {
-    console.log('getChampion => ', error);
+    console.log('getChampion error=> ', error);
     return;
   }
 };
@@ -44,10 +44,24 @@ export const getDetail = async (id: string) => {
     );
 
     const data = await response.json();
-    console.log(data);
+    // console.log(data);
     return data;
   } catch (error) {
-    console.log('getDetail => ', error);
+    console.log('getDetail error=> ', error);
     return;
+  }
+};
+
+// 아이템 목록 정보 가져오기(SSG)
+export const getItem = async () => {
+  const newVersion = await getVersion();
+  try {
+    const response = await fetch(
+      `https://ddragon.leagueoflegends.com/cdn/${newVersion}/data/ko_KR/item.json`
+    );
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log('getItem error => ', error);
   }
 };
