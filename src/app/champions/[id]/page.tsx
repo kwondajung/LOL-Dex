ec290f614279aa696669ca38359953cfa8d17c1f
dajung <kwonkkan@naver.com>
2024-10-07 19:23:22 +0900

    ♻️ Refactor: SSR 렌더링 방식으로 변경

--- src/app/champions/[id]/page.tsx
+++ src/app/champions/[id]/page.tsx
@@ -1,45 +1,50 @@
-'use client';
-
-import { Champion } from '@/types/Champion';
 import { getDetail } from '@/utils/serverApi';
-import { useParams } from 'next/navigation';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
-const championDetailPage = () => {
-  const params = useParams<{ id: string }>();
+type Props = {
+  params: {
+    id: string;
+  };
+};
+
+// 메타데이터
+export function generateMetadata({ params }: Props) {
+  // params에서 id 추출
+  const id = params.id;
+  return {
+    title: `${id}의 상세 페이지`,
+  };
+}
+
+const championDetailPage = async ({ params }: Props) => {
   const championId = params.id;
 
-  const [championDetail, setChampionDetail] = useState<Champion | null>(null);
+  // 챔피언 상세 정보 가져오기
 
-  const fetchChampionDetail = async () => {
-    const detail = await getDetail(championId);
+  const detail = await getDetail(championId);
+  console.log(detail);
 
-    const championData = detail?.data?.[championId];
-    setChampionDetail(championData);
-  };
+  const championData = detail?.data?.[championId];
 
-  useEffect(() => {
-    fetchChampionDetail();
-  }, [championId]);
-
-  if (!championDetail) {
+  if (!championData) {
     return <div>Loading...</div>;
   }
 
-  // console.log('API Data => ', championDetail);
-
   return (
-    <div>
+    <div className="relative">
       <img
         src={`https://ddragon.leagueoflegends.com/cdn/img/champion/splash/${championId}_0.jpg`}
+        className="w-full brightness-50"
       />
-      <p>{championDetail.name}</p>
-      <p>{championDetail.title}</p>
-      <p>{championDetail.blurb}</p>
-      <p>공격력: {championDetail.info.attack}</p>
-      <p>방어력: {championDetail.info.defense}</p>
-      <p>지력: {championDetail.info.magic}</p>
-      <p>난이도: {championDetail.info.difficulty}</p>
+      <div className="absolute top-1/4 text-white pl-48">
+        <p className="text-3xl">{championData.title}</p>
+        <p className="text-white text-8xl font-bold">{championData.name}</p>
+        <p className="w-[700px]">{championData.blurb}</p>
+        <p>공격력: {championData.info.attack}</p>
+        <p>방어력: {championData.info.defense}</p>
+        <p>지력: {championData.info.magic}</p>
+        <p>난이도: {championData.info.difficulty}</p>
+      </div>
     </div>
   );
 };
